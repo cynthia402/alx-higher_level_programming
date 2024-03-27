b521e11e34afed26a11d487ff72bca918cb7ed7b
cynthia402 <cynthiagw10@gmail.com>
2024-03-27 07:36:49 -0700

    all done

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,11 @@
--- Lists all comedy shows in the database hbtn_0d_tvshows.
--- Records are ordered by descending show title.
-SELECT t.`title`
+-- Lists all shows and genres linked to the show from the
+-- database hbtn_0d_tvshows.
+-- Records are ordered by ascending show title and genre name.
+SELECT t.`title`, g.`name`
   FROM `tv_shows` AS t
-       INNER JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
+         LEFT JOIN `tv_show_genres` AS s
+	        ON t.`id` = s.`show_id`
 
-       INNER JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
-       WHERE g.`name` = "Comedy"
- ORDER BY t.`title`;
+       LEFT JOIN `tv_genres` AS g
+              ON s.`genre_id` = g.`id`
+	       ORDER BY t.`title`, g.`name`;
