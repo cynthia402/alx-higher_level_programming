6843e978c6f48bd9b26518911c3118753835caef
cynthia402 <cynthiagw10@gmail.com>
2024-05-10 09:49:57 -0700

    correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
-import urllib.request
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
 
+if __name__ == '__main__':
+    from urllib.request import urlopen
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        content = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf-8')))
